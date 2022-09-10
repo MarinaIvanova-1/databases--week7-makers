8f0f830d188c156f0b8ae07837e0272dcaf48e2d
Marina Ivanova <ubcg62bs@Marinas-MacBook-Pro.local>
2022-09-10 11:06:31 +0100

    Blog posts tags project initial setup

---
+++ blog_posts_tags/app.rb
@@ -0,0 +1,37 @@
+require_relative ''
+
+
+
+class Application
+
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(database_name, io, post_repository, tag_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @post_repository = post_repository
+    @tag_repository = tag_repository
+  end
+
+  def run
+   
+  end
+end
+
+# Don't worry too much about this if statement. It is basically saying "only
+# run the following code if this is the main file being run, instead of having
+# been required or loaded by another file.
+# If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
+if __FILE__ == $0
+  app = Application.new(
+    'blog_posts_tags',
+    Kernel,
+    PostRepository.new,
+    TagRepository.new
+  )
+  app.run
+end
